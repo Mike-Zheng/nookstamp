5f441ccaa5b623c9c3f39a135b31cd05db8e151f
mikezh <mikezhdev@gmail.com>
2020-05-16 18:11:33 +0800

    [add] init

---
+++ precache-manifest.47ef5f73f2d0160cf5f4da7a541af50e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf235fab39cb36927460",
+    "url": "/nook-book-app/css/app.2f14ca42.css"
+  },
+  {
+    "revision": "695dd6800010564535e5",
+    "url": "/nook-book-app/css/chunk-68a17a64.45eaf795.css"
+  },
+  {
+    "revision": "699ac1e5e5e8e8dea1f0",
+    "url": "/nook-book-app/css/chunk-c20e7a76.beafb366.css"
+  },
+  {
+    "revision": "2ca06ccdae538cbbc9f5",
+    "url": "/nook-book-app/css/chunk-vendors.7c28498a.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/nook-book-app/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/nook-book-app/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nook-book-app/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nook-book-app/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nook-book-app/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nook-book-app/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nook-book-app/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9004d858fe858c5d0b5f5efa45dd946d",
+    "url": "/nook-book-app/index.html"
+  },
+  {
+    "revision": "bf235fab39cb36927460",
+    "url": "/nook-book-app/js/app.16e9ebc4.js"
+  },
+  {
+    "revision": "695dd6800010564535e5",
+    "url": "/nook-book-app/js/chunk-68a17a64.1f3d44fe.js"
+  },
+  {
+    "revision": "699ac1e5e5e8e8dea1f0",
+    "url": "/nook-book-app/js/chunk-c20e7a76.e7e79d63.js"
+  },
+  {
+    "revision": "2ca06ccdae538cbbc9f5",
+    "url": "/nook-book-app/js/chunk-vendors.7ad22d4c.js"
+  },
+  {
+    "revision": "96db1f33a064cbf2ae70378ba7f04a17",
+    "url": "/nook-book-app/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/nook-book-app/robots.txt"
+  }
+]);
